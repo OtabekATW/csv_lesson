3d0bc9c5548186069e4e6248c482ee4c2dc1beb6
OtabekATW <otabektursunov468@gmail.com>
2023-12-15 14:24:01 +0500

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 def get_country_column(file_name):
     """
     This function takes a filename as input and returns a list of countries
@@ -6,4 +7,11 @@ def get_country_column(file_name):
     Returns:
         list
     """
-    return 0
+    f = open(file_name)
+    data = csv.reader(f)
+
+    countries = []
+    for i in list(data):
+        countries.append(i[-1])
+
+    return countries
